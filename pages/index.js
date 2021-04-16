609fc2436c1ccd5f1fdc5ff48ffa04a7afb294c4
Ashar Irfan <mrasharirfan@gmail.com>
2021-04-16 23:25:07 +0500

    Add initial working app

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,65 @@
 import Head from 'next/head'
+import useSWR from 'swr'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const fetcher = (...args) => fetch(...args).then(res => res.json())
+
+function getWeather(lat, long) {
+
+  const weatherAPI = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${long}&key=0e39e6553cab44868b50b3c63ac3a781&include=minutely`;
+  const { data, error } = useSWR(weatherAPI, fetcher)
+
+  return {
+    weather: data,
+    isLoading: !error && !data,
+    isError: error
+  }
+}
+
+const getLocaleDate = ( timestamp ) => {
+  return new Intl.DateTimeFormat('en-GB', { dateStyle: 'full' }).format( timestamp * 1000 );
+}
+
+const getWeatherIconUrl = ( code ) => {
+  return `https://www.weatherbit.io/static/img/icons/${code}.png`;
+}
+
+export default function Home({ ip }) {
+
+
+  const {weather, isLoading, isError} = getWeather( ip.lat, ip.lon )
+
+  if ( isError ) return <h1>Error occurred while loading weather data</h1>
+
+  const [ weatherData ] = weather.data;
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Weather App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Weather App
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <h2 className={styles.description}>
+          {
+            isLoading ?
+            'Getting the weather of your city...' :
+            `Today ${getLocaleDate( weatherData.ts )}`
+          }
+        </h2>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div>
+          <img src={getWeatherIconUrl( weatherData.weather.icon )} alt={weatherData.weather.description}/>
         </div>
+
+        <p className={styles.weatherDetails}>
+          {weatherData.city_name} {weatherData.temp}°C {weatherData.weather.description}
+        </p>
       </main>
 
       <footer className={styles.footer}>
@@ -62,4 +74,20 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+
+  const ipRequest = await fetch( 'http://ip-api.com/json' );
+  const ip = await ipRequest.json();
+
+  if ( !ip ) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { ip }
+  };
 }
