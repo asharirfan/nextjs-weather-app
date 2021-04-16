71abdd98df13fb2c6e87f56e8a86ad2fb49a935c
Ashar Irfan <mrasharirfan@gmail.com>
2021-04-17 01:37:15 +0500

    Add container component

---
+++ components/common/Container.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Footer from '@/components/Footer'
+import styles from '../../styles/Home.module.css'
+
+export default function Container({ children }) {
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Weather App</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main id="page-content" className={styles.main}>{ children }</main>
+			<Footer />
+		</div>
+	)
+}
