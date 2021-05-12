1a37b58fb8c6ee2cb153efdfb10d364c5dab973f
Ashar Irfan <mrasharirfan@gmail.com>
2021-05-13 00:48:37 +0500

    Improve weather api to use environment variable

--- functions/getWeather.js
+++ functions/getWeather.js
@@ -3,7 +3,7 @@ import fetcher from './fetcher'
 
 export default function getWeather(lat, long) {
 
-	const weatherAPI = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${long}&key=0e39e6553cab44868b50b3c63ac3a781&include=minutely`;
+	const weatherAPI = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${long}&key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&include=minutely`;
 	const { data, error } = useSWR(weatherAPI, fetcher)
 
 	return {
