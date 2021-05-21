0dc0e31299d1b1ec551808e3322ffd110a142875
Ashar Irfan <mrasharirfan@gmail.com>
2021-05-22 00:01:06 +0500

    Add geodata api to search by location

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,44 @@
+import { useState, useEffect } from 'react'
 import Container from '@/components/common/Container'
 import Weather from '@/components/Weather'
 import styles from '../styles/Home.module.css'
 
 export default function Home({ ip }) {
 
+	const [geoDataInput, setGeoDataInput] = useState('');
+
 	const getGeoData = ( event ) => {
 		// Check for the enter key.
 		// Bail early if it is not pressed.
 		// Search the Geocoding API against the address.
+
+		setTimeout(
+			() => {
+				setGeoDataInput( event.target.value );
+			},
+			1000
+		);
 	}
+
+	useEffect( async () => {
+
+		// Call the Geodata API here.
+		const queryGeoData = async () => {
+			const request = await fetch(
+				`http://api.positionstack.com/v1/forward?access_key=${process.env.NEXT_PUBLIC_POSITION_STACK_KEY}&query=${geoDataInput}`
+			);
+			return request.json();
+		}
+
+		if ( '' !== geoDataInput ) {
+			const getDataResponse = await queryGeoData();
+			console.log('getDataResponse', getDataResponse);
+		}
+
+		// console.log( 'Call the geodata API here' );
+		// console.log( 'geoDataInput', geoDataInput );
+
+	}, [ geoDataInput ] );
 
 	return (
 		<Container>
