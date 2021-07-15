9e4006b9f39ee55615aebd6805709265734665de
Ashar Irfan <mrasharirfan@gmail.com>
2021-07-16 00:28:29 +0500

    Use user browser geo location API

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,45 @@
 import Container from '@/components/common/Container'
 import Weather from '@/components/Weather'
+import { useEffect, useState } from 'react'
 
-export default function Home({ ip }) {
+export default function Home() {
+
+	const [ip, setIp] = useState({});
+
+	const success = (position) => {
+		setIp({ lat: position.coords.latitude, long: position.coords.longitude });
+	};
+
+	const error = () => {};
+
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition(success, error);
+	}, []);
 
 	console.log( ip );
 	return (
 		<Container>
-			<Weather ip={ ip } />
+			{
+			ip.length !== 0 ?
+			<Weather ip={ ip } /> :
+			'Getting the weather of your city...'
+			}
 		</Container>
 	)
 }
 
-export const getStaticProps = async () => {
+// export const getStaticProps = async () => {
 
-	const ipRequest = await fetch( 'http://ip-api.com/json' );
-	const ip = await ipRequest.json();
+// 	const ipRequest = await fetch( 'http://ip-api.com/json' );
+// 	const ip = await ipRequest.json();
 
-	if ( !ip ) {
-		return {
-			notFound: true,
-		}
-	}
+// 	if ( !ip ) {
+// 		return {
+// 			notFound: true,
+// 		}
+// 	}
 
-	return {
-		props: { ip }
-	};
-}
+// 	return {
+// 		props: { ip }
+// 	};
+// }
