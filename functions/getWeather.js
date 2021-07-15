c02bd0d15b5023ded33075be3e7a2fbdf07f4333
Ashar Irfan <mrasharirfan@gmail.com>
2021-07-16 00:28:47 +0500

    Error handling to get weather api call

--- functions/getWeather.js
+++ functions/getWeather.js
@@ -3,6 +3,14 @@ import fetcher from './fetcher'
 
 export default function getWeather(lat, long) {
 
+	if ( lat === undefined || long === undefined ) {
+		return {
+			weather: {},
+			isLoading: true,
+			isError: false
+		}
+	}
+
 	const weatherAPI = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${long}&key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&include=minutely`;
 	const { data, error } = useSWR(weatherAPI, fetcher)
 
