30b9b8d59f613548474a73285661038bc3c221b4
Ashar Irfan <mrasharirfan@gmail.com>
2021-11-18 01:04:55 +0500

    Improve geoLocation API to use Rapid API

--- functions/queryGeoLocationData.js
+++ functions/queryGeoLocationData.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 
 /**
  * Queries geo data of an address from a third party API.
@@ -7,10 +8,30 @@
  */
 const queryGeoLocationData = async ( geoLocation ) => {
 
-	const request = await fetch(
-		`http://api.positionstack.com/v1/forward?access_key=${process.env.NEXT_PUBLIC_POSITION_STACK_KEY}&query=${geoLocation}`
-	);
-	return request.json();
+	const request = {
+		method: 'GET',
+		url: 'https://forward-reverse-geocoding.p.rapidapi.com/v1/search',
+		params: {
+			q: geoLocation,
+			'accept-language': 'en',
+			polygon_threshold: '0.0'
+		},
+		headers: {
+			'x-rapidapi-host': 'forward-reverse-geocoding.p.rapidapi.com',
+			'x-rapidapi-key': '81d4b5315fmshe3aab8d5a756f5ep1f9e54jsn69a66effba45'
+		}
+	};
+
+	let locationData = null;
+
+	try {
+		const response = await axios( request );
+		locationData = response.data;
+	} catch (error) {
+		console.error( 'Geo Location Data API Error', error );
+	}
+
+	return locationData;
 }
 
 export default queryGeoLocationData;
